f135174e0524d0b29b892b1306b84867e4cdeaf5
Fergus Ruston <=>
2017-07-11 21:35:24 +0200

    styling

--- client/app/startup/app.jsx
+++ client/app/startup/app.jsx
@@ -10,11 +10,6 @@ import './style.js';
 const history = createHistory();
 const store = configureStore(history);
 
-document.body.classList.add('bg-blue-to-purple', 'open-sans');
-if (!('ontouchstart' in document.documentElement)) {
-	document.documentElement.classList.add('no-touch');
-}
-
 const app = () => (
 	<Provider store={store}>
 		<Routes history={history} />
--- client/app/startup/style.js
+++ client/app/startup/style.js
@@ -2,3 +2,8 @@ import 'typeface-open-sans';
 import 'typeface-assistant';
 import '../css/site.scss';
 import 'font-awesome/scss/font-awesome.scss';
+
+document.body.classList.add('bg-blue-to-purple', 'open-sans');
+if (!('ontouchstart' in document.documentElement)) {
+	document.documentElement.classList.add('no-touch');
+}
